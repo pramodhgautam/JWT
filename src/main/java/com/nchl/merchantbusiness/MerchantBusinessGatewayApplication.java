52f8dc344f3d9b27ac7e1e829cb74d9228c8b322
Anand Panjiyar <anandpanjiyar@nchl.com.np>
2025-05-20 12:20:31 +0545

    main: merchant business gateway project is initialized

---
+++ src/main/java/com/nchl/merchantbusiness/MerchantBusinessGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nchl.merchantbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantBusinessGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantBusinessGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nchl/merchantbusiness/MerchantBusinessGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchl.merchantbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantBusinessGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
